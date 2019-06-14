2fe26599fe3dab4972c1f3301156a79f2a5a9002
mamaka <basundogel@gmail.com>
2019-06-14 16:09:01 +0800

    add title

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
+	price = int(price)
 	products.append([name, price])
 print(products)
 
@@ -12,3 +13,14 @@ print(products[0][0])
 
 for product in products:
 	print(product[0], '的價格是:', product[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f:     # with 自動close
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n' )
+
+practice
+data = [1, 3, 5, 7, 9]
+with open('numbers.csv', 'w') as f:
+	for number in data:
+		f.write(str(number) + '\n')
