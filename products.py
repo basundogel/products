ae7dd52e261c93e61fe6794130b6721409afaedb
mamaka <basundogel@gmail.com>
2019-06-18 17:53:51 +0800

    'refactor'

--- products.py
+++ products.py
@@ -1,39 +1,59 @@
-products = []
+
+import os  # check if the file is in the same directory as python file
 
 #讀取檔案
-import os  # check if the file is in the same directory as python file
-if os.path.isfile('products.csv'):
-	print('file found')
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding = 'utf-8') as f:
 		for line in f:
 			if '商品,價格' in line:
 				continue        # continue/ break 只能用在迴圈裡
 			name, price = line.strip().split(',')
 			products.append([name, price])
-	print(products)
-else:
-	print('file not found QQ')
+	return products
 
 # 讓使用者輸入
-while True:
-	name = input('請輸入商品名稱:')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格:')
-	price = int(price)
-	products.append([name, price])
-print(products)
-print(products[0][0])
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱:')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格:')
+		price = int(price)
+		products.append([name, price])
+	print(products)   # print(products[0][0])
+	return products
 
 # 印出所有購買紀錄
-for product in products:
-	print(product[0], '的價格是:', product[1])
+def print_products(products):
+	for product in products:
+		print(product[0], '的價格是:', product[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f:     # with 自動close
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n' )
+def write_file(filename, products):
+	with open(filename, 'w', encoding='utf-8') as f:     # with 自動close
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n' )
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('file found')
+		products = read_file(filename)
+	else:
+		print('file not found QQ')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+main()
+
+
+
+
+
 
 # # practice
 # data = [1, 3, 5, 7, 9]
